5c06b4ee0e138e80d4352deb56860ca6a1f7c75d
Vittorio Iocolano <vittorio.iocolano@wrld3d.com>
2021-01-06 12:06:16 +0000

    Add placeholder fragments

--- app/src/main/java/io/github/sirpryderi/astrophotographycalculator/activity/MainActivity.kt
+++ app/src/main/java/io/github/sirpryderi/astrophotographycalculator/activity/MainActivity.kt
@@ -26,7 +26,8 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_time_calculator, R.id.navigation_settings))
+        val topLevelDestinationIds = setOf(R.id.navigation_time_calculator, R.id.navigation_settings, R.id.navigation_ev_calculator, R.id.navigation_help)
+        val appBarConfiguration = AppBarConfiguration(topLevelDestinationIds)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
