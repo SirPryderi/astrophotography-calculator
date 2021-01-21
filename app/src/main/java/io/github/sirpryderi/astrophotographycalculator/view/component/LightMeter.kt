86dda8d858f17fe1ceb0d9f7c91883b6c6a78295
Vittorio Iocolano <vittorio.iocolano@wrld3d.com>
2021-01-21 12:17:58 +0000

    Revamp indicator in ligth meter

--- app/src/main/java/io/github/sirpryderi/astrophotographycalculator/view/component/LightMeter.kt
+++ app/src/main/java/io/github/sirpryderi/astrophotographycalculator/view/component/LightMeter.kt
@@ -6,12 +6,13 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import io.github.sirpryderi.astrophotographycalculator.R
 import io.github.sirpryderi.astrophotographycalculator.view.helper.getStateColour
-import io.github.sirpryderi.astrophotographycalculator.view.helper.getThemeColour
+import kotlin.math.roundToInt
 
 
 class LightMeter(context: Context, attrs: AttributeSet) : android.view.View(context, attrs) {
     private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val strokePaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val strokeWidth = 10f
 
     private var mWidth = width
     private var mHeight = height
@@ -29,14 +30,15 @@ class LightMeter(context: Context, attrs: AttributeSet) : android.view.View(cont
         }
 
     init {
-        textPaint.color = getStateColour(R.color.material_on_surface_emphasis_high_type)
+        minimumHeight = minHeight
+
+        textPaint.color = getStateColour(R.color.material_on_surface_emphasis_medium)
         textPaint.style = Paint.Style.FILL_AND_STROKE
         textPaint.textAlign = Paint.Align.CENTER
         textPaint.textSize = 40f
 
-        strokePaint.color = getThemeColour(R.attr.colorPrimary)
-        strokePaint.strokeWidth = 10f
-        minimumHeight = minHeight
+        strokePaint.color = getStateColour(R.color.colorPrimary)
+        strokePaint.strokeWidth = strokeWidth
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -45,6 +47,7 @@ class LightMeter(context: Context, attrs: AttributeSet) : android.view.View(cont
         // config
         val stops = 4
         val subStops = 2
+        val indicatorStops = 9
         val tickHeight = 30
         val padding = 100
         val indicatorOffset = 10
@@ -56,8 +59,8 @@ class LightMeter(context: Context, attrs: AttributeSet) : android.view.View(cont
 
         val barWidth = mWidth - padding
         val unit = (1f / stops) * ((barWidth) / 2f) // distance between 1 stop
-        val deltaEv = (targetEv - ev).coerceIn(-stops.toFloat(), stops.toFloat())
-        val needleX = x + deltaEv * unit
+        val deltaEv = (targetEv - ev).coerceIn(-stops.toFloat() - 1, stops.toFloat() + 1)
+
         var label = ""
 
         // stop marks
@@ -78,7 +81,17 @@ class LightMeter(context: Context, attrs: AttributeSet) : android.view.View(cont
             }
         }
 
-        canvas?.drawLine(needleX, y + indicatorOffset, needleX, y + indicatorOffset + tickHeight * 2, strokePaint)
+        // indicator
+        strokePaint.strokeWidth = strokeWidth / 2
+        canvas?.drawLine(x, y + indicatorOffset, x, y + indicatorOffset + tickHeight * 2, strokePaint)
+        val end = (deltaEv * indicatorStops).roundToInt()
+        val range = if (deltaEv < 0) (0 downTo end) else (0..end)
+        val offset = 5
+        for (step in range) {
+            val needleX = x + (step / indicatorStops.toFloat()) * unit
+            canvas?.drawLine(needleX, y + indicatorOffset + offset, needleX, y + indicatorOffset + tickHeight * 2 - offset, strokePaint)
+        }
+        strokePaint.strokeWidth = strokeWidth
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
